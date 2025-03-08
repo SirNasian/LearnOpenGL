ad51c86fae4dd6496712c2fa62e3ba80e0143e07
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-08 19:06:05 +0800

    Prefer OpenGL types over native types

--- src/shader_utils.cpp
+++ src/shader_utils.cpp
@@ -12,15 +12,15 @@ const char *getShaderTypeName(const GLenum shader_type) {
 }
 
 namespace ShaderUtils {
-	unsigned int compileShader(GLenum shader_type, const char *shader_source, const char *shader_name) {
-		unsigned int shader = glCreateShader(shader_type);
+	GLuint compileShader(GLenum shader_type, const char *shader_source, const char *shader_name) {
+		GLuint shader = glCreateShader(shader_type);
 		glShaderSource(shader, 1, &shader_source, NULL);
 		glCompileShader(shader);
 
-		int success;
+		GLint success;
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 		if (!success) {
-			char log[512];
+			GLchar log[512];
 			glGetShaderInfoLog(shader, 512, NULL, log);
 			const char *message = "Failed to compile %s shader \"%s\"\n%s\n";
 			SDL_LogError(SDL_LOG_CATEGORY_ERROR, message, getShaderTypeName(shader_type), shader_name, log);
@@ -30,19 +30,18 @@ namespace ShaderUtils {
 		return shader;
 	}
 
-	unsigned int createShaderProgram(const char *vertex_source, const char *fragment_source, const char *shader_name) {
-
-		unsigned int vertex_shader = ShaderUtils::compileShader(GL_VERTEX_SHADER, vertex_source, shader_name);
-		unsigned int fragment_shader = ShaderUtils::compileShader(GL_FRAGMENT_SHADER, fragment_source, shader_name);
-		unsigned int program = glCreateProgram();
+	GLuint createShaderProgram(const char *vertex_source, const char *fragment_source, const char *shader_name) {
+		GLuint vertex_shader = ShaderUtils::compileShader(GL_VERTEX_SHADER, vertex_source, shader_name);
+		GLuint fragment_shader = ShaderUtils::compileShader(GL_FRAGMENT_SHADER, fragment_source, shader_name);
+		GLuint program = glCreateProgram();
 		glAttachShader(program, vertex_shader);
 		glAttachShader(program, fragment_shader);
 		glLinkProgram(program);
 
-		int success;
+		GLint success;
 		glGetProgramiv(program, GL_LINK_STATUS, &success);
 		if (!success) {
-			char log[512];
+			GLchar log[512];
 			glGetProgramInfoLog(program, 512, NULL, log);
 			const char *message = "Failed to links shader program \"%s\"\n%s\n";
 			SDL_LogError(SDL_LOG_CATEGORY_ERROR, message, shader_name, log);
--- src/shader_utils.hpp
+++ src/shader_utils.hpp
@@ -4,8 +4,8 @@
 #include <glad/glad.h>
 
 namespace ShaderUtils {
-	unsigned int compileShader(GLenum shader_type, const char *shader_source, const char *shader_name = "unknown");
-	unsigned int createShaderProgram(const char *vertex_source, const char *fragment_source, const char *shader_name = "unknown");
+	GLuint compileShader(GLenum shader_type, const char *shader_source, const char *shader_name = "unknown");
+	GLuint createShaderProgram(const char *vertex_source, const char *fragment_source, const char *shader_name = "unknown");
 }
 
 #endif
